ff004fd7c271be57819c087526146fa64fe9109b
seiei-n <solehamugoiyo@gmail.com>
2023-09-23 11:13:46 +0900

    Add init

---
+++ script/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ../webapp/go
+make all
+
+sudo systemctl stop --now isucholar.go.service
+sudo systemctl disable isucholar.go.service
+sudo systemctl daemon-reload
+sudo systemctl start --now isucholar.go.service
+sudo systemctl enable isucholar.go.service
+
+sudo systemctl restart nginx
+sudo systemctl restart mysql
+
