a5d399158be617852f35f236a7461bf39f4dc9d0
Daaaai0809 <dai.tsuruga@80and.co>
2023-09-23 13:07:32 +0900

    add: registrationにindex追加

---
+++ sql/4_create_index.sql
@@ -0,0 +1,6 @@
+create index idx_teacher_id on courses (teacher_id);
+create index idx_course_id on classes (course_id);
+create index idx_course_id_announcements on announcements (course_id);
+create index idx_submission_class_id on submissions (class_id);
+create index idx_course_id_registrations on registrations (course_id);
+create index idx_user_id_registrations on registrations (user_id);
