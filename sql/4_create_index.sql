89d60e6d8ef918b2b409234fd69a038f2b0b9cb8
Daaaai0809 <dai.tsuruga@80and.co>
2023-09-23 16:37:31 +0900

    うおおおおお

--- sql/4_create_index.sql
+++ sql/4_create_index.sql
@@ -4,6 +4,6 @@ create index idx_annonecement_id on unread_announcements (announcement_id, is_de
 create index idx_user_id on unread_announcements (user_id, is_deleted);
 create index idx_id_announcements on announcements (id desc);
 create index idx_course_id_announcements on announcements (course_id);
-create index idx_submission_class_id on submissions (class_id);
+create index idx_submission_class_id on submissions (user_id, class_id);
 create index idx_course_id_registrations on registrations (course_id);
 create index idx_user_id_registrations on registrations (user_id);
