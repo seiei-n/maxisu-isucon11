85497aae8eae2dfc9947633f56112f2a524671cf
Daaaai0809 <dai.tsuruga@80and.co>
2023-09-23 16:43:20 +0900

    huoooo

--- go/main.go
+++ go/main.go
@@ -624,14 +624,21 @@ func (h *handlers) GetGrades(c echo.Context) error {
 
 		// この科目を履修している学生のTotalScore一覧を取得
 		var totals []int
-		query := "SELECT IFNULL(SUM(`submissions`.`score`), 0) AS `total_score`" +
-			" FROM `users`" +
-			" JOIN `registrations` ON `users`.`id` = `registrations`.`user_id`" +
-			" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
-			" LEFT JOIN `classes` ON `courses`.`id` = `classes`.`course_id`" +
-			" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
-			" WHERE `courses`.`id` = ?" +
-			" GROUP BY `users`.`id`"
+		query := "SELECT IFNULL(SUM(submissions.score), 0) AS total_score"
+			"	FROM users" +
+			"	JOIN registrations ON users.id = registrations.user_id"+
+			"	JOIN courses ON registrations.course_id = courses.id"+
+			"	WHERE courses.id = ?"+
+			"	AND users.id IN ("+
+			"		SELECT DISTINCT users.id"+
+			"		FROM users"+
+			"		JOIN registrations ON users.id = registrations.user_id"+
+			"		JOIN courses ON registrations.course_id = courses.id"+
+			"		LEFT JOIN classes ON courses.id = classes.course_id"+
+			"		LEFT JOIN submissions ON users.id = submissions.user_id AND submissions.class_id = classes.id"+
+			"		WHERE courses.id = ?"+
+			"	)"+
+			"	GROUP BY users.id"
 		if err := h.DB.Select(&totals, query, course.ID); err != nil {
 			c.Logger().Error(err)
 			return c.NoContent(http.StatusInternalServerError)
